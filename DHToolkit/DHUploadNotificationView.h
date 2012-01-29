ab3d2e9bbb876c6d3c63b37d0a59b10c586d7222
Tim Shi <timgshi@gmail.com>
2012-01-29 02:41:02 -0800

    loading view created and added, won't disappear on success though

---
+++ DHToolkit/DHUploadNotificationView.h
@@ -0,0 +1,13 @@
+//
+//  DHUploadNotificationView.h
+//  DHToolkit
+//
+//  Created by Tim Shi on 1/29/12.
+//  Copyright (c) 2012 www.timshi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHUploadNotificationView : UIView
+
+@end
---
+++ DHToolkit/DHUploadNotificationView.m
@@ -0,0 +1,31 @@
+//
+//  DHUploadNotificationView.m
+//  DHToolkit
+//
+//  Created by Tim Shi on 1/29/12.
+//  Copyright (c) 2012 www.timshi.com. All rights reserved.
+//
+
+#import "DHUploadNotificationView.h"
+
+@implementation DHUploadNotificationView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
