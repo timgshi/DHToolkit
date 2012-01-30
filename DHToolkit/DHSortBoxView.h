78b20f3b083a2fd0f102e03bb3a2d14aacbaabee
Tim Shi <timgshi@gmail.com>
2012-01-29 16:51:33 -0800

    notifications display and disappear correctly. should be no uploading or downloading bugs. started implementation of sort view

---
+++ DHToolkit/DHSortBoxView.h
@@ -0,0 +1,13 @@
+//
+//  DHSortBoxView.h
+//  DHToolkit
+//
+//  Created by Tim Shi on 1/29/12.
+//  Copyright (c) 2012 www.timshi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHSortBoxView : UIView
+
+@end
---
+++ DHToolkit/DHSortBoxView.m
@@ -0,0 +1,31 @@
+//
+//  DHSortBoxView.m
+//  DHToolkit
+//
+//  Created by Tim Shi on 1/29/12.
+//  Copyright (c) 2012 www.timshi.com. All rights reserved.
+//
+
+#import "DHSortBoxView.h"
+
+@implementation DHSortBoxView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
