4a9bc798757ccd689c183ae9f3d95a2eb76033cf
Tim Shi <timgshi@gmail.com>
2012-02-03 16:56:06 -0800

    started implementation for detail view. will be using a container vc to hold the image and meta-info vcs

---
+++ DHImageDetailContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DHImageDetailContainerViewController.h
+//  DHToolkit
+//
+//  Created by Tim Shi on 2/3/12.
+//  Copyright (c) 2012 www.timshi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHImageDetailContainerViewController : UIViewController
+
+@end
