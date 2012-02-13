ebe99177d12cd482c40d1cc58ca1352327621de9
Tim Shi <timgshi@gmail.com>
2012-02-13 00:43:50 -0800

    starting work on the comment view. added vc+nib for header of comment view

---
+++ DHImageDetailMetaHeaderVC.h
@@ -0,0 +1,24 @@
+//
+//  DHImageDetailMetaHeaderVC.h
+//  DHToolkit
+//
+//  Created by Tim Shi on 2/13/12.
+//  Copyright (c) 2012 www.timshi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHImageDetailMetaHeaderVC : UIViewController
+
+
+@property (strong, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (strong, nonatomic) IBOutlet UILabel *dateHeaderLabel;
+@property (strong, nonatomic) IBOutlet UILabel *dateLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weatherHeaderLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weatherLabel;
+@property (strong, nonatomic) IBOutlet UILabel *levelLabel;
+@property (strong, nonatomic) IBOutlet UIView *levelBarView;
+@property (strong, nonatomic) IBOutlet MKMapView *locationMapView;
+
+@end
